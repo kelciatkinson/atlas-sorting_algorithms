4828e511dbfb73f276e4f8c142d2396740e7d542
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-09 00:55:59 +0000

    betty

--- sort.h
+++ sort.h
@@ -5,11 +5,7 @@
 #include <stdio.h>
 
 /**
- * data structures
- */
-
-/**
- * struct listint_s - Doubly linked list node
+ * struct listint_s- Doubly linked list node
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
@@ -22,9 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * prototypes
- */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
